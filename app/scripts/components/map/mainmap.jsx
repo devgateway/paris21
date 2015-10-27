4353b199da025c7f5493c11620064864fe052eec
ddimunzio <ddimunzio@developmentgateway.org>
2015-10-27 18:15:38 -0300

    Test load regions and some colors

--- app/scripts/components/map/mainmap.jsx
+++ app/scripts/components/map/mainmap.jsx
@@ -1,21 +1,74 @@
 import React, { PropTypes } from 'react';
-import {  Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import {  Map, TileLayer, Marker, Popup, GeoJson } from 'react-leaflet';
 import { connect } from 'reflux';
 import BoundsMap from '../leaflet/bounds-map';
 import SpinnerModal from '../misc/spinner-modal';
 import { load } from '../../actions/education';
 import Education from '../../stores/education';
+import { loadRegions } from '../../actions/regions';
+import Regions from '../../stores/regions';
+import { loadData } from '../../actions/primarygri';
+import PrimaryGri from '../../stores/primarygri';
 
 require('stylesheets/map/map');
 
 const MainMap = React.createClass({
    mixins: [
     connect(Education, 'education'),
+    connect(Regions, 'regions'),
+    connect(PrimaryGri, 'primarygri'),
   ],
   componentWillMount() {
+    loadData();
+    loadRegions();
     load();
   },
   render() {
+    var primaryGriData= <div/>;
+    if (this.state.regions.regions){
+        primaryGriData=<GeoJson map="MainMap" data={this.state.regions.regions} style={
+          function(feature){
+            console.log(feature.properties.ADM1_NAME);
+          if ('Dakar' == feature.properties.ADM1_NAME) {
+            return {
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 1,
+                fillColor: '#a50f15'
+            };
+        }else if ('Kolda' == feature.properties.ADM1_NAME) {
+            return {
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 1,
+                fillColor: '#de2d26'
+            };
+        }else if ('Kaffrine' == feature.properties.ADM1_NAME) {
+            return {
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 1,
+                fillColor: '#fcae91'
+            }; 
+          } else {
+            return {
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 1,
+                fillColor: '#fee5d9'
+            };
+          }
+        }
+    }/>
+    }
     return (
       <div className="main-map">
         <div className="map-container">
@@ -30,6 +83,7 @@ const MainMap = React.createClass({
                    </Marker>
                    ];
                 })}
+              {primaryGriData}
            </BoundsMap>
            <div className="messages">
               There are {this.state.education.length} Education structures loaded.
