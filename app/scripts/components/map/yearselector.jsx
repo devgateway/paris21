a608685d9f9f4781690721414226e072e3c8ff38
ddimunzio <ddimunzio@developmentgateway.org>
2016-03-09 15:18:22 -0300

    Remove 2010

--- app/scripts/components/map/yearselector.jsx
+++ app/scripts/components/map/yearselector.jsx
@@ -12,7 +12,7 @@ const yearSelector = React.createClass({
 
   getInitialState() {
     return {
-      value: '2010',
+      value: '2011',
     };
   },
 
@@ -25,11 +25,11 @@ const yearSelector = React.createClass({
   render() {
     return (
          <select className="selectable" id="year" onChange={this.change} value={this.state.years}>
-              <option value="2010">2010</option>
               <option value="2011">2011</option>
               <option value="2012">2012</option>
               <option value="2013">2013</option>
               <option value="2014">2014</option>
+              <option value="2015">2015</option>
            </select>
         );
   },
